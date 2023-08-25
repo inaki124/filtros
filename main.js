75b0acd0382e52c0e7386cd398e6fa066fe71970
inaki124 <113951615+inaki124@users.noreply.github.com>
2023-08-24 19:09:38 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 function setup() {
     canvas=createCanvas(400, 250);
     canvas.center();
-
-  }
   
+    video=createCapture(VIDEO);
+    video.size(400, 250);
+    video.hide();
+  
+    poseNet=ml5.poseNet(video, modelLoaded);
+  
+    poseNet.on('pose',gotPoses);
+  }
+  function modelLoaded() {
+    console.log('poseNet esta inicializado');
+  }
   function preload() {
     
   }
   function draw() {
- 
+   image(video,0,0,400,250);
   }
-
-  function click(){
-
+  function click() {
+    save('myfilterimage.png');
+  }
+  function gotPoses(results) {
+    if (results.length>0) {
+    console.log(results);
+    console.log('nose x= '+results[0].pose.nose.x);
+    console.log('nose y= '+results[0].pose.nose.y);
+    }
   }
