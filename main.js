4d41543b77e39ca6a9f613ac61dad8dc5bd1030f
inaki124 <113951615+inaki124@users.noreply.github.com>
2023-08-24 18:42:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas=createCanvas(400, 250);
+    canvas.center();
+
+  }
+  
+  function preload() {
+    
+  }
+  function draw() {
+ 
+  }
+
+  function click(){
+
+  }
